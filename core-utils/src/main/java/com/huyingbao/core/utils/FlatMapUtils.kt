e3b9ffbee67580aceabf80999f320126a854330d
liujunfeng <1049380320@qq.com>
2020-06-03 07:00:23 +0800

    升级 rxjava3

--- core-utils/src/main/java/com/huyingbao/core/utils/FlatMapUtils.kt
+++ core-utils/src/main/java/com/huyingbao/core/utils/FlatMapUtils.kt
@@ -1,39 +1,8 @@
 package com.huyingbao.core.utils
 
-import io.reactivex.Observable
-import io.reactivex.ObservableSource
-import io.reactivex.Observer
-import io.reactivex.functions.Function
-import retrofit2.HttpException
+import io.reactivex.rxjava3.core.ObservableSource
+import io.reactivex.rxjava3.core.Observer
 import retrofit2.Response
-
-/**
- * FlatMap转化工具类
- *
- * Created by liujunfeng on 2019/1/1.
- */
-object FlatMapUtils {
-    /**
-     * 验证接口返回数据是正常，取出[Response.body]并传递。
-     */
-    @SuppressWarnings("unchecked")
-    fun <T, R> verifyResponse(): Function<T, Observable<R>> {
-        return Function {
-            if (it is Response<*>) {
-                if (it.isSuccessful) {
-                    //接口调用成功，向下传递body
-                    return@Function Observable.just((it as Response<R>).body())
-                } else {
-                    //接口调用失败，向下传递异常
-                    return@Function Observable.error(HttpException(it))
-                }
-            } else {
-                //返回值类型转换异常
-                return@Function Observable.error(ClassCastException(it.toString()))
-            }
-        }
-    }
-}
 
 /**
  * 将Response转化为实体数据
